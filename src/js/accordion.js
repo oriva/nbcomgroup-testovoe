b6b07f43dcc49d55eb51dbfbbc68d8c06b5e7fb0
Oriva <weelman93@gmail.com>
2020-12-18 04:08:09 +0300

    prefinal ver

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -19,23 +19,101 @@ const getJSON = (url) => {
 	});
 };
 
+const showAccordion = (elemToggled, elemParent, fixHeight) => {
+	const showList = () => {
+		elemToggled.classList.add('active');
+		elemParent.classList.add('show');
+		elemToggled.style.height = 'auto';
+
+		let height = `${elemToggled.clientHeight}px`;
+
+		elemToggled.style.height = '0px';
+
+		setTimeout(() => {
+			elemToggled.style.height = height;
+		}, 0);
+	};
+
+	if (fixHeight === 'Y') {
+		if (elemToggled.classList.contains('active')) {
+			showList();
+		}
+	} else if (!elemToggled.classList.contains('active')) {
+		showList();
+	} else {
+		elemToggled.style.height = '0px';
+
+		elemToggled.addEventListener('transitionend', () => {
+			elemToggled.classList.remove('active');
+			elemParent.classList.remove('show');
+		}, {
+			once: true,
+		});
+
+	}
+};
+
 const formData = (() => {
-	let formElem;
+	const infoContainer = document.querySelector('.configurator__list');
+	const valueClass = 'list-value';
 	let dataInput = {};
+
+	const newElement = (inputTitle, inputKey, inputValue) => {
+		const divElem = document.createElement('div');
+		divElem.id = `info-${inputKey}`;
+		dataInput[inputKey] = divElem;
+		divElem.innerHTML = `<b>${inputTitle}: </b><span class="${valueClass}">${inputValue}</span>`;
+		infoContainer.append(divElem);
+
+		const accordionList = document.querySelector('.configurator__list');
+		const accordion = document.querySelector('.configurator__total-block');
+		showAccordion(accordionList, accordion, 'Y');
+	};
+
+	const changeElement = (inputKey, inputValue) => {
+		console.log(dataInput[inputKey]);
+		dataInput[inputKey].querySelector(`.${valueClass}`).innerHTML = inputValue;
+	};
+
+	const deleteElement = (inputKey) => {
+		dataInput[inputKey].parentNode.removeChild(dataInput[inputKey]);
+		delete dataInput[inputKey];
+
+		const accordionList = document.querySelector('.configurator__list');
+		const accordion = document.querySelector('.configurator__total-block');
+		showAccordion(accordionList, accordion, 'Y');
+	};
 
 	const changeInfo = (inputElement) => {
 		let inputKey = inputElement.name;
-		dataInput[inputKey] = inputElement.value;
-		console.log(dataInput);
+		let inputTitle = inputElement.dataset.codeTitle;
+		let inputValue = inputElement.value;
+		if (inputElement.dataset.add === 'Y') {
+			if (dataInput[inputKey]) {
+				deleteElement(inputKey);
+			}
+		} else if (!dataInput[inputKey]) {
+			newElement(inputTitle, inputKey, inputValue);
+		} else if (dataInput[inputKey] !== inputElement.value) {
+			changeElement(inputKey, inputValue);
+		}
+	};
+
+	const initAccordionEvent = () => {
+		const accordionName = document.querySelector('.accordion__name_total');
+		const accordionList = document.querySelector('.configurator__list');
+		const accordion = document.querySelector('.configurator__total-block');
+		accordionName.addEventListener('click', () => {
+			showAccordion(accordionList, accordion);
+		});
 	};
 
 	return {
-		init: (formSelector, listenerElem, inputParent) => {
-			formElem = document.querySelector('.form-configurator');
-			console.log(listenerElem);
+		init: (formSelector, listenerElem, classInputParent) => {
+			initAccordionEvent();
 			listenerElem.addEventListener('click', (e) => {
-				if (e.target.closest(inputParent)) {
-					changeInfo(e.target.closest(inputParent).querySelector('input'));
+				if (e.target.closest(classInputParent)) {
+					changeInfo(e.target.closest(classInputParent).querySelector('input'));
 				}
 			});
 		},
@@ -43,39 +121,14 @@ const formData = (() => {
 })();
 
 const accordionStartFn = (() => {
-	const showAccordion = (elemToggled, elemParent) => {
-		if (!elemToggled.classList.contains('active')) {
-			elemToggled.classList.add('active');
-			elemParent.classList.add('show');
-			elemToggled.style.height = 'auto';
-
-			let height = `${elemToggled.clientHeight}px`;
-
-			elemToggled.style.height = '0px';
-
-			setTimeout(() => {
-				elemToggled.style.height = height;
-			}, 0);
-		} else {
-			elemToggled.style.height = '0px';
-
-			elemToggled.addEventListener('transitionend', () => {
-				elemToggled.classList.remove('active');
-				elemParent.classList.remove('show');
-			}, {
-				once: true,
-			});
-		}
-	};
-
-	const accordionListGenerate = (dataItem, listName, key) => {
+	const accordionListGenerate = (dataItem, listName, key, dataTitle) => {
 		const accordionListIn = document.createElement('div');
 		// console.log(dataItem);
 		accordionListIn.innerHTML = `
 		<div class="radio-btn accordion__item ${dataItem.add ? 'disabled' : ''}">
-		<input name="${listName}" id="accordion-${listName}-${dataItem.id}" type="radio" ${key === 0 ? 'checked' : ''} value="${dataItem.text}">
-		<label for="accordion-${listName}-${dataItem.id}">${dataItem.text}
-		${dataItem.extend ? `<span>${dataItem.extend}</span>` : ''}</label>
+			<input name="${listName}" id="accordion-${listName}-${dataItem.id}" type="radio" ${key === 0 ? 'checked' : ''} value="${dataItem.text}" data-code-title="${dataTitle}" ${dataItem.add === false ? 'data-add="Y"' : ''}>
+			<label for="accordion-${listName}-${dataItem.id}">${dataItem.text}
+			${dataItem.extend ? `<span>${dataItem.extend}</span>` : ''}</label>
 		</div>`;
 
 		return accordionListIn;
@@ -106,7 +159,7 @@ const accordionStartFn = (() => {
 			} else {
 				subKey = 2;
 			}
-			accordionList.append(accordionListGenerate(dataItem, listName, subKey));
+			accordionList.append(accordionListGenerate(dataItem, listName, subKey, data.title));
 		});
 		accordion.append(accordionList);
 
